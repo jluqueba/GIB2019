283c78720770ac1654e8e14b8e07aae4a5385a5f
Jose Luque <joluqueb@microsoft.com>
2019-03-18 15:09:14 +0100

    Implementación de las Demos 1 y 2

---
+++ Demo02/Messages/Commands/PlaceOrder.cs
@@ -0,0 +1,12 @@
+﻿using NServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo02.Messages.Commands
+{
+    public class PlaceOrder : ICommand
+    {
+        public string OrderId { get; set; }
+    }
+}
